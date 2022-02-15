689d94ee8e1ce926d23836e5d254e768c7e2b209
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-02-15 16:31:27 +0900

    Use ID for URL path instead name

--- static/js/router.js
+++ static/js/router.js
@@ -16,20 +16,31 @@ page("/channels/:channelName", (ctx) => {
   document.getElementById("header-channel-name").innerText = channelName;
   loadChannel(channelName);
 });
-page("/channels/:channelId/:threadName", (ctx) => {
+page("/channels/:channelId/:threadId", (ctx) => {
   const channelId = ctx.params.channelId;
-  const threadName = ctx.params.threadName;
-  const channelName = `${channelId}/${threadName}`;
+  const threadId = ctx.params.threadId;
+
+  const threads = Object.values(window.categories)
+    .flat()
+    .filter((x) => x.threads)
+    .map((x) => x.threads)
+    .flat();
+  const thread = threads.find((x) => x.id === threadId);
 
   for (const activeMenu of document.querySelectorAll(
     ".menu-list a.is-active"
   )) {
     activeMenu.classList.remove("is-active");
   }
-  document.getElementById(`sidebar-${channelName}`).classList.add("is-active");
+  document
+    .getElementById(`sidebar-${channelId}/${threadId}`)
+    .classList.add("is-active");
 
-  document.getElementById("header-channel-name").innerText = threadName;
-  loadChannel(channelName);
+  document.getElementById(
+    "header-channel-name"
+  ).innerText = `${thread.categoryName}/${thread.channelName}`;
+
+  loadChannel(`${thread.channelId}/${thread.channelName}`);
 });
 
 page();
