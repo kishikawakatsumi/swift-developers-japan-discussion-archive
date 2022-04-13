070400260f530c46afe18e2a93f481146f367f15
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-04-13 22:37:13 +0900

    Use stable version

--- dev/get_thread_messages.js
+++ dev/get_thread_messages.js
@@ -15,13 +15,13 @@ fs.readdirSync(root, { withFileTypes: true }).forEach((dirent) => {
       const parentId = thread.parentId;
       const threadId = thread.threadId;
 
-      execSync(`docker run --rm -v $PWD/data/json/${parentId}:/output tyrrrz/discordchatexporter:2.33.2 export \
+      execSync(`docker run --rm -v $PWD/data/json/${parentId}:/output tyrrrz/discordchatexporter:stable export \
   -t ${process.env.DISCORD_TOKEN} \
   -c ${threadId} \
   -o "/output/%P|${index}|%T|%C|%t|%c.json" \
   -f Json`);
 
-      execSync(`docker run --rm -v $PWD/data/html/${parentId}:/output tyrrrz/discordchatexporter:2.33.2 export \
+      execSync(`docker run --rm -v $PWD/data/html/${parentId}:/output tyrrrz/discordchatexporter:stable export \
   -t ${process.env.DISCORD_TOKEN} \
   -c ${threadId} \
   -o "/output/%c.html" \
